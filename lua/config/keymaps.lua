5516c92e1220504589ec46425d4ead2f1855bbfb
Cliff Chew <clifchew84@gmail.com>
2025-08-18 11:07:31 +0800

    Change number lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -59,3 +59,13 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.textwidth = 80
 vim.wo.colorcolumn = "80"
+
+-- Set the color of all line numbers (LineNr)
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#A0A0A0" }) -- Example: light grey
+
+-- Set the color of the line number on the current line (CursorLineNr)
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFFFFF", bold = true }) -- Example: white and bold
+
+-- If using relative line numbers, set colors for LineNrAbove and LineNrBelow
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#FFA07A" }) -- Example: light orange
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#98FB98" }) -- Example: pale green
